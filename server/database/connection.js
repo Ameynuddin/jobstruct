8c33161659ae16bf7f1ff39bb3e94a7cb980bea2
Ameynuddin <md.ameynuddin@gmail.com>
2024-08-30 21:50:34 +0800

    edited db conn

--- server/database/connection.js
+++ server/database/connection.js
@@ -2,6 +2,14 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config({path:'./.env'})
 
-mongoose.connect(process.env.DBURL)
-.then(db => console.log("Database is connected"))
-.catch(error=> console.log("Database is not connected", error))
+// mongoose.connect(process.env.DBURL)
+// .then(db => console.log("Database is connected"))
+// .catch(error=> console.log("Database is not connected", error))
+
+mongoose.connect(process.env.DBURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Error connecting to MongoDB:', err));
