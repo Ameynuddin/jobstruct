faca974f16c2d5cf3fc98de1591586fbc29ee35a
Ameynuddin <md.ameynuddin@gmail.com>
2024-09-26 00:03:24 +0800

    updated sidebar

--- client/src/admincomponents/Sidebar.js
+++ client/src/admincomponents/Sidebar.js
@@ -8,7 +8,7 @@ import { IoMenu } from "react-icons/io5";
 import { FaPowerOff } from "react-icons/fa6";
 import { useData } from '../API/ApiContext'
 
-function NavButton({ to, icon, label, onClick }) {
+const NavButton = ({ to, icon, label, onClick }) => {
   const location = useLocation();
   const isActive = location.pathname === to;
 
@@ -82,13 +82,14 @@ function Sidebar() {
 
   return (
     <>
-      <aside className={`bg-gradient-to-br from-gray-800 to-gray-900 fixed inset-0 z-50 my-4 ml-4 h-[calc(100vh-32px)] w-72 rounded-xl transition-transform duration-300 ${isSidebarOpen ? 'translate-x-0' : '-translate-x-80'} xl:translate-x-0`}>
+      <aside className={`bg-gradient-to-br from-gray-800 to-gray-800/95 fixed inset-0 z-50 my-4 ml-4 h-[calc(100vh-32px)] w-72 rounded-lg transition-transform duration-300 ${isSidebarOpen ? 'translate-x-0' : '-translate-x-80'} xl:translate-x-0`}>
 
         <div className="relative border-b border-white/20">
           <a className="flex items-center gap-4 py-6 px-8" href="/adminhome">
             <h6 className="block antialiased tracking-normal font-sans text-base font-semibold leading-relaxed text-white text-center">JobStruct</h6>
           </a>
 
+          {/* X (close sidebar) button */}
           <button
             onClick={toggleSidebar}
             className="middle none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-8 max-w-[32px] h-8 max-h-[32px] rounded-lg text-xs text-white hover:bg-white/10 active:bg-white/30 absolute right-0 top-0 grid rounded-br-none rounded-tl-none xl:hidden"
@@ -162,9 +163,9 @@ function Sidebar() {
       ) : (
         <button
           onClick={toggleSidebar}
-          className="fixed top-4 left-4 z-40 xl:hidden p-2 bg-gray-800 rounded-md"
+          className="fixed top-9 left-6 z-40 xl:hidden p-2 bg-gray-800/85 rounded-md"
         >
-          <IoMenu color='white' />
+          <IoMenu className='text-white text-2xl shadow-lg hover:animate-spin'/>
         </button>
       )}
     </>
